4891670b7bfe14ad9a7af4fd12037d2bc096926b
杨庆杰 <1534760657@qq.com>
2024-07-03 17:36:32 +0800

    增加遥控页面和手势

--- robot/lib/trainmode/train_mode_controller.dart
+++ robot/lib/trainmode/train_mode_controller.dart
@@ -8,6 +8,7 @@ import 'package:tennis_robot/trainmode/robot_move_view.dart';
 import 'package:tennis_robot/trainmode/robot_route_view.dart';
 import 'package:tennis_robot/trainmode/train_mode_total_view.dart';
 import 'package:tennis_robot/views/button_switch_view.dart';
+import 'package:tennis_robot/views/remote_control_view.dart';
 
 /// 训练模式
 class TrainModeController extends StatefulWidget {
@@ -31,6 +32,7 @@ class _TrainModeControllerState extends State<TrainModeController> {
     return Scaffold(
       backgroundColor: Constants.darkControllerColor,
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         // crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Container(
@@ -62,7 +64,10 @@ class _TrainModeControllerState extends State<TrainModeController> {
                 ),
                 Container(
                   alignment: Alignment.center,
-                  child: mode == 3 ?ButtonSwitchView(leftTitle: 'Retrieve', rightTitle: 'Pause') :ModeSwitchView(),
+                  child: mode == 3
+                      ? ButtonSwitchView(
+                          leftTitle: 'Retrieve', rightTitle: 'Pause')
+                      : ModeSwitchView(),
                 ),
                 Container(
                   margin: EdgeInsets.only(right: 16),
@@ -81,53 +86,61 @@ class _TrainModeControllerState extends State<TrainModeController> {
                   child: RobotRouteView(),
                   // child: RobotMoveView(),
                 )
-              : Container(
-                  margin: EdgeInsets.only(top: 18),
-                  child: SizedBox(
-                    height: 60,
-                  ),
-                ),
-
-          Container(
-            margin: EdgeInsets.only(top: 10),
-            // child: Image(image: AssetImage('images/connect/select_area_line.png'),
-            // width: Constants.screenWidth(context),
-            // height: (Constants.screenWidth(context) -58*2) * (813 / 522) / 2 + 180,
-            // ),
-            //
-
-            child: Stack(
-              clipBehavior: Clip.none,
-              children: [
-                Container(
-                  child: Image(
-                    image: AssetImage('images/connect/select_area_line.png'),
-                  ),
-                  width: Constants.screenWidth(context),
-                  height: (Constants.screenWidth(context) - 58 * 2) *
-                          (813 / 522) /
-                          2 +
-                      180,
-                ),
-
-                mode == 1 ?
-                Positioned(
-                    left: 24,
-                    top: 100,
-                    child: TrainModeTotalView()
+              : mode != 3
+                  ? Container(
+                      margin: EdgeInsets.only(top: 18),
+                      child: SizedBox(
+                        height: 60,
+                      ),
+                    )
+                  : Container(),
+          mode == 3
+              ? Container(
+                  margin: EdgeInsets.only(left: 20, right: 20),
+                  child: RemoteControlView(),
                 )
-                    : Positioned(child: Container(),),
-              ],
-            ),
-          ),
+              : Container(
+                  margin: EdgeInsets.only(top: 10),
+                  // child: Image(image: AssetImage('images/connect/select_area_line.png'),
+                  // width: Constants.screenWidth(context),
+                  // height: (Constants.screenWidth(context) -58*2) * (813 / 522) / 2 + 180,
+                  // ),
+                  //
+
+                  child: Stack(
+                    clipBehavior: Clip.none,
+                    children: [
+                      Container(
+                        child: Image(
+                          image:
+                              AssetImage('images/connect/select_area_line.png'),
+                        ),
+                        width: Constants.screenWidth(context),
+                        height: (Constants.screenWidth(context) - 58 * 2) *
+                                (813 / 522) /
+                                2 +
+                            180,
+                      ),
+                      mode == 1
+                          ? Positioned(
+                              left: 24, top: 100, child: TrainModeTotalView())
+                          : Positioned(
+                              child: Container(),
+                            ),
+                    ],
+                  ),
+                ),
           Container(
             margin: EdgeInsets.only(top: 70),
             child: RobotFunctionSwitchView(
-              onTapClick: (index){
+              onTapClick: (index) {
                 modeChange(index);
               },
             ),
           ),
+          SizedBox(
+            height: 60,
+          )
         ],
       ),
     );
--- robot/lib/views/remote_control_view.dart
+++ robot/lib/views/remote_control_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tennis_robot/constant/constants.dart';
 import 'package:tennis_robot/utils/color.dart';
+
 class RemoteControlView extends StatefulWidget {
   const RemoteControlView({super.key});
 
@@ -9,18 +10,61 @@ class RemoteControlView extends StatefulWidget {
 }
 
 class _RemoteControlViewState extends State<RemoteControlView> {
+  Offset position = Offset(0, 0); // 初始位置为试图A的中心
+
   @override
   Widget build(BuildContext context) {
     return Container(
       width: Constants.screenWidth(context) - 40,
       height: Constants.screenWidth(context) - 40,
       decoration: BoxDecoration(
-        color: hexStringToColor('#2B2C30'),
-        borderRadius: BorderRadius.circular((Constants.screenWidth(context) - 40)/2.0)
-      ),
+          color: hexStringToColor('#2B2C30'),
+          borderRadius: BorderRadius.circular(
+              (Constants.screenWidth(context) - 40) / 2.0)),
       child: Stack(
         children: [
-          Image(image: AssetImage('images/connect/remote_control.png'))
+          Center(
+            child: Image(
+              image: AssetImage('images/connect/remote_control.png'),
+              height: Constants.screenWidth(context) - 40 - 96,
+            ),
+          ),
+          Positioned(
+              left: (Constants.screenWidth(context) - 40 - 44) / 2.0,
+              top: (Constants.screenWidth(context) - 40 - 44) / 2.0,
+              child: GestureDetector(
+                onPanUpdate: (details) {
+                  // 更新圆点的位置
+                  setState(() {
+                    position += details.delta;
+                    // 限制圆点在试图A内部移动
+                    position = Offset(
+                      position.dx.clamp(
+                          -((Constants.screenWidth(context) - 40 )/ 2.0),
+                          ((Constants.screenWidth(context) - 40)) / 2.0),
+                      position.dy.clamp(
+                          -((Constants.screenWidth(context) - 40) / 2.0),
+                          ((Constants.screenWidth(context) - 40) / 2.0)),
+                    );
+                    print('(Constants.screenWidth(context) - 40 / 2.0)=${((Constants.screenWidth(context) - 40 )/ 2.0)}');
+                    print('details.delta;=${position}');
+
+                  });
+                },
+                onPanEnd: (details) {
+                  // 手指松开时，将圆点移动回试图A的中心
+                  setState(() {
+                    position = Offset(0, 0);
+                  });
+                },
+                child: Transform.translate(
+                  offset: position,
+                  child: Image(
+                    image: AssetImage('images/connect/control_point.png'),
+                    height: 44,
+                  ),
+                ),
+              ))
         ],
       ),
     );
